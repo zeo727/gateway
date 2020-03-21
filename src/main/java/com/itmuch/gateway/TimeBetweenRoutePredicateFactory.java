97d14531f2046ca448f2c369d4f4e360b180da97
likexin <likexin>
2020-03-21 11:11:09 +0800

    API网关-Spring Cloud Gateway

---
+++ src/main/java/com/itmuch/gateway/TimeBetweenRoutePredicateFactory.java
@@ -0,0 +1,47 @@
+package com.itmuch.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.handler.predicate.AbstractRoutePredicateFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class TimeBetweenRoutePredicateFactory
+extends AbstractRoutePredicateFactory<TimeBetweenConfig> {
+    public TimeBetweenRoutePredicateFactory() {
+        super(TimeBetweenConfig.class);
+    }
+
+    @Override
+    public Predicate<ServerWebExchange> apply(TimeBetweenConfig config) {
+        LocalTime start = config.getStart();
+        LocalTime end = config.getEnd();
+        return exchange ->{
+            LocalTime now = LocalTime.now();
+            return now.isAfter(start) && now.isBefore(end);
+        };
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList("start","end");
+    }
+
+    /**
+     * 路由配置时间格式底层
+     * 默认时间格式：org.springframework.format.support.DefaultFormattingConversionService#addDefaultFormatters
+     * 时间格式注册：org.springframework.format.datetime.standard.DateTimeFormatterRegistrar#registerFormatters
+     * @param args
+     */
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+        System.out.println(formatter.format(LocalTime.now()));
+    }
+}
